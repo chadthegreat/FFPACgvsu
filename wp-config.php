f86697d335b0a57253a515a03030c47381dff5d2
Chris Schaefer <schaefer007@gmail.com>
2016-03-31 13:39:44 -0400

    Config

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'ffpac');
 define('DB_PASSWORD', '2sJrDmQnhahzSCjM');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'barnescode.db');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
